0e4e93cde5a0f94daee203d62ffa7b56f522935e
Rohit Mendhekar <103194502+rohit99878@users.noreply.github.com>
2023-04-04 22:52:12 +0530

    Add files via upload

---
+++ dict01.py
@@ -0,0 +1,14 @@
+dict = {
+    'employee1' : {'id' : 1,
+     'Name' : 'Rohit Mendhekar',
+     'Salary' : 10000,
+     'Dept' : 'IT',
+     'Address' : 'Pune'},
+     'employee2' : {'id' : 2,
+      'id' : 'Rohit Mendhekar',
+      'Salary' : 10000,
+      'Dept' : 'IT',
+      'Address' : 'Pune'}}
+
+for i in dict.items():
+    print(i)
