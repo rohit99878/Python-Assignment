723b39a0dfe74e4a541a96a7044ed564e5ce0abf
Rohit Mendhekar <103194502+rohit99878@users.noreply.github.com>
2023-07-23 13:20:15 +0530

    Add files via upload

---
+++ Projects/Calculator/calculator.py
@@ -0,0 +1,50 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+def print_menu():
+    print("Calculator Menu:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+def calculator():
+    while True:
+        print_menu()
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice == 5:
+                print("Exiting the calculator.")
+                break
+            elif choice not in [1, 2, 3, 4]:
+                print("Invalid choice. Please choose a number from the menu.")
+                continue
+            
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            
+            if choice == 1:
+                print("Result:", add(num1, num2))
+            elif choice == 2:
+                print("Result:", subtract(num1, num2))
+            elif choice == 3:
+                print("Result:", multiply(num1, num2))
+            elif choice == 4:
+                print("Result:", divide(num1, num2))
+        except ValueError:
+            print("Invalid input. Please enter valid numbers.")
+
+if __name__ == "__main__":
+    calculator()
